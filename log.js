0fd434d054c61f0e9d39acda1ee318d071e99d3e
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-02 14:50:01 +0700

    Rename to log.js complete second kata

---
+++ log.js
@@ -0,0 +1,21 @@
+// function calculateDiscount(price, category) {
+//     let discount
+//     if (category === 'electronics') {
+//         discount = price * 0.10
+//         console.log(category, discount)
+//     } else {
+//         discount = price * 0.05
+//         console.log(category, discount);
+        
+//     }
+//     return discount
+// }
+
+// calculateDiscount(10, 'electronics');
+// calculateDiscount(10, 'food');
+
+function multiplyNumbers(a, b) {
+    return a * b
+}
+
+console.log(multiplyNumbers(5,3));
