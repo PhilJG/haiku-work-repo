2681dd7f0df3de0a6324e9e3a2c7d7a44473e1ba
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-06 12:39:40 +0700

    complete kata #2

--- objects.js
+++ objects.js
@@ -39,12 +39,36 @@
 // The object will have a nested structure, with an object inside the main object.
 // You need to access the email property inside the nested object and return its value.
 
-function getNestedProperty(property) {
-    console.log(typeof property.contact.email);
-    return property.contact.email
+// function getNestedProperty(property) {
+//     console.log(typeof property.contact.email);
+//     return property.contact.email
+// }
+
+// getNestedProperty({'contact':{'email':'alice@example.com'}})	
+// // 'alice@example.com'
+// getNestedProperty({'contact':{'email':'bob@example.com'}})	
+// // 'bob@example.com'
+
+// Calculating Total Price from Array of Objects
+// Create a function named calculateTotalPrice.
+// This function should take an array of objects as its parameter.
+// Each object in the array represents an item, and each item has a price property.
+// The function should calculate the total price of all items in the array by summing up their individual prices and return the total sum.
+
+function calculateTotalPrice(arr) {
+    let total = 0
+    for (let el of arr) {
+        total = total + el.price
+    }
+    return total
 }
 
-getNestedProperty({'contact':{'email':'alice@example.com'}})	
-// 'alice@example.com'
-getNestedProperty({'contact':{'email':'bob@example.com'}})	
-// 'bob@example.com'
+let a = [{"name": "Bag", "price": 90}, {"name": "Shoes", "price": 100}]
+console.log(
+    calculateTotalPrice(a) // 190
+);
+
+let b = [{"name": "Car", "price": 1000}, {"name": "Apple", "price": 5}, {"name": "Laptop", "price": 500}]
+console.log(
+    calculateTotalPrice(b) // 1505
+);
