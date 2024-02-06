35c19801fd187da8fb60b5f3abe976ea671d929f
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-06 13:15:48 +0700

    complete kata 3

--- objects.js
+++ objects.js
@@ -55,20 +55,63 @@
 // Each object in the array represents an item, and each item has a price property.
 // The function should calculate the total price of all items in the array by summing up their individual prices and return the total sum.
 
-function calculateTotalPrice(arr) {
+// function calculateTotalPrice(arr) {
+//     let total = 0
+//     for (let el of arr) {
+//         total = total + el.price
+//     }
+//     return total
+// }
+
+// let a = [{"name": "Bag", "price": 90}, {"name": "Shoes", "price": 100}]
+// console.log(
+//     calculateTotalPrice(a) // 190
+// );
+
+// let b = [{"name": "Car", "price": 1000}, {"name": "Apple", "price": 5}, {"name": "Laptop", "price": 500}]
+// console.log(
+//     calculateTotalPrice(b) // 1505
+// );
+
+
+// Summing Positive Numbers in Nested Arrays
+// Your task is to create a function named sumPositiveNumbers.
+// This function should take an object as its parameter.
+// Within this object, there are two properties array1 and array2, each containing an array of numbers.
+// The function should calculate the sum of all positive numbers from both arrays combined and return the total sum.
+
+function sumPositiveNumbers(obj) {
     let total = 0
-    for (let el of arr) {
-        total = total + el.price
+
+    function positiveValue(num){
+        if(num >= 0) {
+          return num
+        }
+        return 0
     }
+
+    for (let el of obj.array1) {
+      total = total + positiveValue(el)
+    }
+
+    for (let el of obj.array2) {
+        total = total + positiveValue(el)       
+    }
+
     return total
 }
 
-let a = [{"name": "Bag", "price": 90}, {"name": "Shoes", "price": 100}]
+let a = {
+    array1: [1, -3, 5, 7],
+    array2: [10, -2, 4, -5]
+}
 console.log(
-    calculateTotalPrice(a) // 190
+    sumPositiveNumbers(a) // 27
 );
 
-let b = [{"name": "Car", "price": 1000}, {"name": "Apple", "price": 5}, {"name": "Laptop", "price": 500}]
-console.log(
-    calculateTotalPrice(b) // 1505
-);
+
+let b = {
+    array1: [10, 20, -9],
+    array2: [200, 1]
+}
+sumPositiveNumbers(b) // 241
