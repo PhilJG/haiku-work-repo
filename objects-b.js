81d8d28ccb8a35a2fdfadc72a3f831f790f059bc
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-07 13:10:00 +0700

    complete methods 1

--- objects-b.js
+++ objects-b.js
@@ -151,9 +151,55 @@
 // A query has been made to a database and a user object was returned, including the password property, which should not be shared for security reasons.
 // Create a function removePassword that takes a user in the format of an object, deletes the password from the user, then returns the user with all its fields but not the password.
 
-function removePassword(user) {
-    delete user.password    
-    return user
+// function removePassword(user) {
+//     delete user.password    
+//     return user
+// }
+
+// removePassword({'name':'Jack','age':45,'password':'777888'})	
+
+// ///////////////////////////////
+// Delete Non-Encrypted Passwords
+// //////////////////////////////
+// A query has been made to a database and an array of users was returned, including the password property for each user object.
+// Some of the passwords are encrypted (the user has an encrypted: true property), while others are not.
+// Create a function removePasswords that takes a list of users in the format of an array of objects, deletes the password field only from users whose password is NOT encrypted, as well as the encrypted property, then returns the updated users array.
+
+// function removePasswords(users) {
+//     for (let user of users) {
+//         if(!user.encrypted) {
+//             delete user.password
+//             delete user.encrypted
+//         }
+//     }
+//     console.log(users);
+    
+//     return users
+// }
+
+
+
+// console.log (removePasswords([
+//     {'name':'Jack','age':45,'password':'123456'},
+//     {'name':'Bob','age':27,'password':'9!4%2!@','encrypted':true}
+// ])	)
+// removePasswords([
+//     {'name':'Alice','password':'Alice'}
+// ])	
+
+////////////////////////
+// Create a Method
+//////////////////////////
+
+// Create an object greeter and add a method sayHello to it.
+// The method should return 'Hello World'
+
+const greeter = {
+    sayHello: function(){
+        console.log('Hello World');
+        
+        return 'Hello World'
+    }
 }
 
-removePassword({'name':'Jack','age':45,'password':'777888'})	
+greeter.sayHello() // 'Hello World'
