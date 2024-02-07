4eebc44dc40b462bf956a02f1c16b0cca7d94772
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-07 13:18:50 +0700

    complete methods 3

--- objects-b.js
+++ objects-b.js
@@ -209,15 +209,38 @@
 // Create an object greeter.
 // In the object, create a method sayHello that takes a parameter, then returns a string as per the tests below.
 
-const greeter = {
-    sayHello: function(name){
-        if (!name) {
-            return 'Hello guest'
-        }        
-        return 'Hello ' + name
-    }
-}
+// const greeter = {
+//     sayHello: function(name){
+//         if (!name) {
+//             return 'Hello guest'
+//         }        
+//         return 'Hello ' + name
+//     }
+// }
 
-greeter.sayHello('John') // 'Hello John'
-greeter.sayHello('Alice') // 'Hello Alice'
-greeter.sayHello() // 'Hello guest'
+// greeter.sayHello('John') // 'Hello John'
+// greeter.sayHello('Alice') // 'Hello Alice'
+// greeter.sayHello() // 'Hello guest'
+
+// //////////////////////
+// Counter
+// /////////////////////
+
+// // Create an object counter with 2 methods: increase and decrease.
+// const counter = {
+//     increase: function (input) {
+//         input.count++
+//         console.log(input);
+//     },
+//     decrease: function (input) {
+//         input.count--
+//         console.log(input);
+//     }
+// }
+
+// // Each method takes an object, as per the below tests, and returns the object with internal count value either increased by 1 or decreased by 1.
+
+// counter.increase({ count: 1 }) // { count: 2 }
+// counter.increase({ count: 10 }) // { count: 11 }
+// counter.decrease({ count: 1 }) // { count: 0 }
+// counter.decrease({ count: 10 }) // { count: 9 }
