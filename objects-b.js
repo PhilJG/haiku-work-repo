abd7d9b45831bd64ddf40cbe6d9498b345d0df03
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-07 12:02:23 +0700

    complete 2 of adding

--- objects-b.js
+++ objects-b.js
@@ -81,3 +81,31 @@
 // Your task is to create a JavaScript function named addAge.
 // This function should take two parameters: an object and a number.
 // The function should add the provided number to the object as age and return the updated object.
+
+// function addAge(obj, num) {
+//     obj.age = num   
+//     return obj 
+// }
+
+// addAge({'name':'Alice'},25)
+// addAge({'name':'Bob','job':'coder'},51)
+
+// ///////////////////////////////
+// Add Ages
+// //////////////////////////////
+// Your task is to create a JavaScript function named addAges.
+// This function should take two parameters: an array of objects and an array of numbers of equal length.
+// The function should add the numbers in the second array to the objects as age, following the corresponding index, and return the updated array of objects.
+
+function addAges(objs, nums) {
+    let counter = 0
+    for (let obj of objs) {
+        obj.age = nums[counter]
+        counter++
+    }
+    console.log(obj);
+    
+    return obj
+}
+
+addAges([{'name':'Tom'},{'name':'Tim'},{'name':'Tam'}],[30,31,32])
