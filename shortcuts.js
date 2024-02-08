663db441f231c7c14c3588453eb069c4aa1fb19f
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-08 13:43:29 +0700

    complete ternary operators

--- shortcuts.js
+++ shortcuts.js
@@ -58,3 +58,24 @@
 // // test	expected
 // numerToBoolean(5)	// true
 // numerToBoolean(-30)	// false
+
+///////////////
+// Half or Double
+// Create a function halfOrDouble that takes an array of numbers.
+// The function should then check the value of each number and, if it's greater than 10, it should halve it, otherwise it should double it.
+// The function should then return the updated array.
+// Use a ternary operator.
+
+function halfOrDouble(array) {
+    let result = []
+    let index = 0
+    for (let number of array) {   
+        result[index] = number > 10 ? number/2 : number*2
+        index++     
+    }
+    return result
+}
+
+halfOrDouble([10,5,2,14])	//  [20,10,4,7]
+halfOrDouble([-20,1,70,2,9,0,16])	//  [-40,2,35,4,18,0,8]
+halfOrDouble([])	// []
