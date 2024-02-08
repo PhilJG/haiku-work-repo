018fb8c4b3e8f4fd8a75d252bfe0642de64968ad
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-08 13:58:25 +0700

    finish arrow function 3

--- shortcuts.js
+++ shortcuts.js
@@ -84,13 +84,28 @@
 // Function to Arrow
 // Convert the following function into an arrow function.
 
-const add = (a, b) => a + b
+// const add = (a, b) => a + b
 
 
-console.log(
-    add(5,2)	//7
-    );
+// console.log(
+//     add(5,2)	//7
+//     );
 
-    console.log(
-        add(1,2)	//3
-        );
+//     console.log(
+//         add(1,2)	//3
+//         );
+
+///////////////////////
+// Fix the Arrow Function 1
+// The following arrow function is broken. Can you fix it?
+
+// const subtract = (a, b) => a - b
+
+// console.log(subtract(5,2)	);
+// console.log(subtract(10,1)	);
+
+// const multiply = (a, b) => a * b
+
+// console.log(multiply(10,2)	);
+// console.log(multiply(9,3)	);
+
