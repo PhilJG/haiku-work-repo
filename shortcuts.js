c2571068d9b81b7ad685201a9bab5541a7edb45b
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-08 15:07:06 +0700

    complete ternary

--- shortcuts.js
+++ shortcuts.js
@@ -131,10 +131,32 @@
 // Concatenation to Template Literal
 // Convert the following String Concatenation into a Template Literal.
 
-function greet(age, city) {
-    return `Hello, I'm ${age} years old and I'm from ${city}!`
-}
+// function greet(age, city) {
+//     return `Hello, I'm ${age} years old and I'm from ${city}!`
+// }
 
-console.log(greet(20,'Tokyo'));
+// console.log(greet(20,'Tokyo'));
+
+//////////////////
+// Time Greet
+// Create an arrow function greetTime that takes a parameter time (a number).
+// Inside the function, create a second arrow function checkTime that takes a parameter t (a number).
+// Within the checkTime function, use a ternary operator to check if t is less than 12, if it is, return 'morning', otherwise return 'evening'.
+// Within the greetTime function, return a template literal that includes the invokation of the function checkTime, passing time as a parameter, to then return either of the following strings:
+
+// 'Good morning'
+// 'Good evening'
+
+// function greetTime(number) {
+//     const checkTime = t => {
+//     return t < 12 ? 'morning' : 'evening'
+//     }
+//     return `Good ${checkTime(number)}`
+// }
+
+// console.log(
+//     greetTime(10));
+//     console.log(greetTime(13));
+    
 
 
