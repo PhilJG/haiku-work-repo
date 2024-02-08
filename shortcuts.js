7909f7a9f72ae7e3b197fd15a9712e839b59c893
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-08 13:52:06 +0700

    arrow function 1

--- shortcuts.js
+++ shortcuts.js
@@ -66,16 +66,31 @@
 // The function should then return the updated array.
 // Use a ternary operator.
 
-function halfOrDouble(array) {
-    let result = []
-    let index = 0
-    for (let number of array) {   
-        result[index] = number > 10 ? number/2 : number*2
-        index++     
-    }
-    return result
-}
+// function halfOrDouble(array) {
+//     let result = []
+//     let index = 0
+//     for (let number of array) {   
+//         result[index] = number > 10 ? number/2 : number*2
+//         index++     
+//     }
+//     return result
+// }
 
-halfOrDouble([10,5,2,14])	//  [20,10,4,7]
-halfOrDouble([-20,1,70,2,9,0,16])	//  [-40,2,35,4,18,0,8]
-halfOrDouble([])	// []
+// halfOrDouble([10,5,2,14])	//  [20,10,4,7]
+// halfOrDouble([-20,1,70,2,9,0,16])	//  [-40,2,35,4,18,0,8]
+// halfOrDouble([])	// []
+
+// /////////////////
+// Function to Arrow
+// Convert the following function into an arrow function.
+
+const add = (a, b) => a + b
+
+
+console.log(
+    add(5,2)	//7
+    );
+
+    console.log(
+        add(1,2)	//3
+        );
