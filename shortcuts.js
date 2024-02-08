a0523681f57301a8e31929edadbcb18f6358ce85
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-08 12:04:55 +0700

    complete 1

---
+++ shortcuts.js
@@ -0,0 +1,10 @@
+// Shortcut in a Loop
+// Use a shortcut in the following function to replace the increment of i in the loop and the increment of sum.
+
+function timer() {
+  let sum = 0
+  for (let i = 0; i < 10; i++) {
+    sum++
+  }
+  return sum
+}
