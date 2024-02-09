524468e61426393aaa383ab969037d7a6719e576
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-09 13:33:11 +0700

    complete 4 string methods

--- native-methods.js
+++ native-methods.js
@@ -110,14 +110,49 @@
 // This function should take two parameters: a string and a number.
 // It should return true if the length of the string is greater than the given number, and false otherwise.
 
-function isLongerThan(string, num) {
-    return string.length > num ? true : false
-}
+// function isLongerThan(string, num) {
+//     return string.length > num ? true : false
+// }
 
-// test	expected
-console.log(
-    isLongerThan('hello',3), //true
-    isLongerThan('hello',5), //false
-    isLongerThan('',1), //false
-    isLongerThan('',0), //false
-    );
+// // test	expected
+// console.log(
+//     isLongerThan('hello',3), //true
+//     isLongerThan('hello',5), //false
+//     isLongerThan('',1), //false
+//     isLongerThan('',0), //false
+//     );
+
+/////////////////////
+// String into Array
+// Create a JavaScript function named splitStringIntoArray.
+// This function should take two parameters: a string and a delimiter. It should divide the string into an array of substrings, separated by the specified delimiter, and return the resulting array.
+// function splitStringIntoArray(string, delimiter) {
+//     return string.split(delimiter)
+// }
+
+// // test	expected
+// console.log(
+//     splitStringIntoArray('apple,banana,cherry',','),	//['apple','banana','cherry']
+//     splitStringIntoArray('Hello World',' '),	//['Hello','World']
+//     splitStringIntoArray('one-two-three','-'),	//['one','two','three']
+//     );
+
+/////////////////////
+// Search
+// Create a function search that takes two parameters: an array of strings, and a string keyword.
+// The function should return the string from the array that contains the keyword.
+// Assume that there is only 1 string containing the keyword.
+// function search(arr, keyword) {
+//     let result
+//     for (let string of arr) {
+//         if (string.includes(keyword)) {
+//             result = string
+//         }
+//     }
+//     return result
+// }
+
+// console.log(
+// search(['hello world','good morning'],'world'),	//'hello world'
+// search(['123456','911'],'9'),	//'911'
+// );
