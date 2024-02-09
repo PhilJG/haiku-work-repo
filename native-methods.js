1c78536a6e5c61b2d33ec08df7b2bce8aaa15fad
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-09 11:10:58 +0700

    complete 2 of array methods

--- native-methods.js
+++ native-methods.js
@@ -2,19 +2,43 @@
 // Return Length
 // Create a function returnLength that takes an array as its only parameter, then returns the length of the array as a number, or 'the array is empty' if there are no elements in the array
 
-function returnLength(arr) {
-    return arr.length === 0 ? 'array is empty' : arr.length
+// function returnLength(arr) {
+//     return arr.length === 0 ? 'array is empty' : arr.length
+// }
+
+
+// console.log(
+    
+//     returnLength([1,2,3,4])	//  4
+//     );
+// console.log(
+    
+//     returnLength(['a'])	//  1
+//     );
+// console.log(
+//     returnLength([])	//  'the array is empty'
+//     );
+
+// //////////////
+// Sports Lenghts
+// Create a function addSportsLengths that takes an array of objects.
+// Each object contains an array of sports.
+// The function should add a property sportsLength to each object, with the length of its sports array.
+
+function addSportsLengths(arr) {
+    for (let player of arr) {
+         player.sportsLength = player.sports.length
+    }
+   return arr
 }
 
+// test	expected
+console.log(
+    addSportsLengths([{'name':'Bob','sports':[]}])	
+    );
+//[{'name':'Bob','sports':[],'sportsLength':0}]
 
 console.log(
-    
-    returnLength([1,2,3,4])	//  4
+    addSportsLengths([{'name':'John','sports':['football']},{'name':'Alice','sports':['tennis','golf']}])
     );
-console.log(
-    
-    returnLength(['a'])	//  1
-    );
-console.log(
-    returnLength([])	//  'the array is empty'
-    );
+//[{'name':'John','sports':['football'],'sportsLength':1},{'name':'Alice','sports':['tennis','golf'],'sportsLength':2}]
