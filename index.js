7179dab11d1b72f90583785d3e87d1aa740ac4eb
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-14 11:59:17 +0700

    complete modules 1-3

---
+++ index.js
@@ -0,0 +1,15 @@
+// import timezone from "./constants.js";
+
+// console.log(timezone);
+
+import {pi, tau} from "./constants.js"
+
+// console.log(pi);
+// console.log(tau);
+
+import { calcCircleArea } from "./math/functions.js"
+
+console.log(
+    calcCircleArea(10, pi), // 314
+    calcCircleArea(5, pi) // 78.5
+    );
---
+++ math/functions.js
@@ -0,0 +1,5 @@
+import { pi, tau } from "../constants.js"
+
+export function calcCircleArea(r, pi) {
+    return r*r*pi
+}
