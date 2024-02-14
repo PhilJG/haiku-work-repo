be8679bd6107a4a0efb077f43cd02659876804c4
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-14 10:44:11 +0700

    Update async-await.js

--- async-await.js
+++ async-await.js
@@ -30,12 +30,55 @@
 // Collect the error
 // The function getResults() returns an error. Collect the error and log it in the console.
 
-const search = async () => {
-  try {
-      await getResults()
-  } catch (err) {
-    console.log(`Try harder ${err}`);
-  }
+// const search = async () => {
+//   try {
+//       await getResults()
+//   } catch (err) {
+//     console.log(`Try harder ${err}`);
+//   }
+// }
+
+// search()
+
+///////////////////////
+// Collect any error
+// One of the 2 functions getResults() and getPages() returns an error. You don't know which one. Collect any of the 2 errors and log them in the console.
+
+// const search = async () => {
+//     try {
+//         let results = await getResults()
+//     } catch (error) {
+//         console.log('Error getting results', error);
+//     }
+
+//     try {
+//         let pages = await getPages()
+//     } catch (error){
+//         console.log('Error getting pages', error);
+//     }
+// }
+
+// console.log(search());
+
+// Get ToDo
+// Create an async function getTodo that:
+
+// receives a parameter number
+// uses axios to make a request to the url https://jsonplaceholder.typicode.com/todos/1 replacing the last 1 with the parameter function
+// returns the data of the response object
+// Test your code on the Simulation Platform.
+
+const getTodo = async () => {
+    let response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    return response.data
 }
 
-search()
+// Test the following:
+console.log(
+
+    getTodo(1), // { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
+    getTodo(2), // { userId: 1, id: 2, title: 'quis ut nam facilis et officia qui', completed: false }
+    getTodo(3), // { userId: 1, id: 3, title: 'fugiat veniam minus', completed: false }
+    )
+    
+    ``
